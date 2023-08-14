ea1a208a31a743de3ba49470a7d73db2da032955
Slava B <slbabin@gmail.com>
2023-08-14 20:30:39 +0000

    Fix empty divs bugs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,6 +37,9 @@ function repoInformationHTML(){
 }
 
 function fetchGitHubInformation(event){
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
     var username = $("#gh-username").val();
     if (!username){
         $("#gh-user-data").html('<h2> Please enter a GitHub username</h2>');
@@ -68,3 +71,4 @@ function fetchGitHubInformation(event){
         }
     )
 }
+$(document).ready(fetchGitHubInformation);
