7404e5596fde678381fb7cb7bef0d3fe68a01349
Slava B <slbabin@gmail.com>
2023-08-15 10:48:11 +0000

    Implement emailjs integration on contact page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,16 @@
-console.log("heelo");
+function sendMail(contactForm) {
+    emailjs.send("project_requests","Slava", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
